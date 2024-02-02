5f11ab79b234dee6b1514163dd9524fd1cd7f78d
KwonCheolHyeon <ch970314@naver.com>
2024-02-02 10:11:17 +0900

    우주 관련 에셋 변경 SapceGameManager : 여러개의 오브젝트를 하나로 통일 하는 작업 SpaceSphereScript : 우주에서 사용할 구체 스크립트 SpaceTongsMoveScript : 우주에서 집게 움직임 스크립트

--- Assets/Scripts/SpaceScene/SpaceGameManager.cs
+++ Assets/Scripts/SpaceScene/SpaceGameManager.cs
@@ -1,18 +1,295 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
 using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System.Text;
 
 public class SpaceGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static SpaceGameManager instance = null;
+    //풀링 오브젝트 관련
+    [SerializeField]
+    private GameObject spaceObjects;
+    private Queue<SpaceSphereScript> poolingObjectQueues;
+    private Vector3 initSphereVec3 = new Vector3(0, 16, 0);
+    //풀링 오브젝트 관련
+
+    //오브젝트 세팅 관련
+    private float[] sizes = new float[11];
+    [SerializeField]
+    private Mesh[] meshes;
+    //오브젝트 세팅 관련
+
+    //집게 세팅 관련
+    [SerializeField]
+    private GameObject TongsMove;
+    private bool isTongs = false;
+    //집게 세팅 관련
+
+    //게임 점수 관련
+    [SerializeField]
+    private TextMeshProUGUI nowScoreText;
+    [SerializeField]
+    private TextMeshProUGUI hightScroreText;
+    private int gameScore = 0;
+    //게임 점수 관련
+
+    //게임 오버 관련
+    private bool gameoverState = false;
+    [SerializeField]
+    private GameObject gameOverPanel;
+
+    //카메라 관련
+    private SpaceCameraMoveScript cameraScr;
+    //
+
+    //스테이지 흔들기
+    [SerializeField]
+    private GameObject stage;
+
+    public int Score // Property
+    {
+        get { return gameScore; } // Get accessor
+        set { gameScore = value; } // Set accessor
+    }
+
+    public bool TongState
+    {
+        get { return isTongs; }
+        set { isTongs = value; }
+    }
+
+    public bool GameOverState
+    {
+        get { return gameoverState; }
+        set { gameoverState = value; }
+    }
+
+    public static SpaceGameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    public Mesh[] GetSapceMesh() 
+    {
+        return meshes;
+    }
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        float prevSize = 0.1f;
+        for (int index = 0; index < 11; index++)
+        {
+            sizes[index] = prevSize + 0.01f;
+            prevSize = sizes[index];
+        }
+
+        gameOverPanel.SetActive(false);
+        cameraScr = Camera.main.GetComponent<SpaceCameraMoveScript>();
+        InitializeSphere(18);
+    }
+
     void Start()
     {
-        
+        nowScoreText.text = 0 + "";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TriggerStageShake();
+        }
+    }
+
+    private void InitializeSphere(int initCount)
+    {
+        poolingObjectQueues = new Queue<SpaceSphereScript>();
+        for (int i = 0; i < initCount; i++)
+        {
+            poolingObjectQueues.Enqueue(CreateNewObject(0, sizes[0]));
+        }
+    }
+
+    private SpaceSphereScript CreateNewObject(int _type, float _size)
+    {
+        SpaceSphereScript newObj = Instantiate(spaceObjects).AddComponent<SpaceSphereScript>();
+        newObj.SettingSphere(_type, _size);
+        newObj.gameObject.SetActive(false);
+        newObj.transform.position = initSphereVec3;
+        newObj.transform.SetParent(transform);
+        newObj.GetComponent<SphereCollider>().enabled = false;
+        newObj.SpaceMeshSetting();
+
+        return newObj;
+    }
+    public SpaceSphereScript GetObject(int _type)//오브젝트 불러오기
+    {
+        if (gameoverState)
+            return null;
+
+        float _size = SpaceGameManager.instance.sizes[_type];
+        if (poolingObjectQueues.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueues.Dequeue();
+            obj.GetComponent<SpaceSphereScript>().SettingSphere(_type, _size);
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewObject(_type, _size);
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj;
+        }
+    }
+
+    public SpaceSphereScript GetLevelUpObject(int _type)//레벨업시에만 불러옴
+    {
+        float _size = SpaceGameManager.instance.sizes[_type];
+        if (Instance.poolingObjectQueues.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueues.Dequeue();
+            obj.GetComponent<SpaceSphereScript>().SettingSphere(_type, _size);
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewObject(_type, _size);
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj;
+        }
+    }
+
+    public void ReturnObject(SpaceSphereScript obj, int _type)
+    {
+
+        obj.GetComponent<Rigidbody>().useGravity = true; //HideSphereObject 꺼준 2개의 컴포넌트들을 다시 켜주고 active를 끔
+
+        obj.GetComponent<SphereCollider>().enabled = false;
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        poolingObjectQueues.Enqueue(obj);
+    }
+
+    public void SphereBottomTrue()//바닥에 닿았을 때
+    {
+        TongsMove.GetComponent<SpaceTongsMoveScript>().NextSphererInforMation();
+    }
+
+    public void SetGameScore(int type)
+    {
+
+        switch (type)
+        {
+            case 0:
+                gameScore += 2;
+                break;
+            case 1:
+                gameScore += 6;
+                break;
+            case 2:
+                gameScore += 12;
+                break;
+            case 3:
+                gameScore += 20;
+                break;
+            case 4:
+                gameScore += 30;
+                break;
+            case 5:
+                gameScore += 42;
+                break;
+            case 6:
+                gameScore += 56;
+                break;
+            case 7:
+                gameScore += 72;
+                break;
+            case 8:
+                gameScore += 90;
+                break;
+            case 9:
+                gameScore += 110;
+                break;
+            case 10:
+                gameScore += 132;
+                break;
+            default:
+                Debug.LogError("Game Manager SetGameScore type 오류");
+                break;
+        }
+
+        nowScoreText.text = gameScore + "";
+
+    }
+
+    public void GameOver()
+    {
+        if (gameoverState == false)
+        {
+            gameoverState = true;
+            gameOverPanel.SetActive(true);
+            cameraScr.GameOverCameraMove();
+        }
+    }
+
+    public void CameraShake()
+    {
+        cameraScr.TriggerCameraShake();
+    }
+
+    public void TriggerStageShake()
+    {
+        float duration = 0.3f;
+        float magnitude = 0.2f;
+        StartCoroutine(ShakeObject(duration, magnitude));
+    }
+
+    public IEnumerator ShakeObject(float duration, float magnitude)
+    {
+        Vector3 originalPos = stage.transform.localPosition;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = UnityEngine.Random.Range(-1f, 1f) * magnitude;
+            float y = UnityEngine.Random.Range(-1f, 1f) * magnitude;
+            float z = UnityEngine.Random.Range(-1f, 1f) * magnitude;
+
+            stage.transform.localPosition = new Vector3(originalPos.x + x, originalPos.y + y, originalPos.z + z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        stage.transform.localPosition = originalPos;
     }
 }
--- Assets/Scripts/SpaceScene/SpaceTongsMoveScript.cs
+++ Assets/Scripts/SpaceScene/SpaceTongsMoveScript.cs
@@ -1,18 +1,335 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TongsMoveScript1 : MonoBehaviour
+public class SpaceTongsMoveScript : MonoBehaviour
 {
+    private Camera mainCamera;
+    private SpaceCameraMoveScript mCameraScript;
+    private bool _isMoving = true;
+    private float moveSpeed = 4.0f;
+    private float minX, maxX, minZ, maxZ;
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    private SpaceSphereScript heldSphere = null;
+
+    //연속 떨구는거 딜레이 주기 위한 bool값
+    private bool isReleasing = false;
+    private bool canDropSphere = true;
+    //연속 떨구는거 딜레이 주기
+    //랜덤 시스템관련
+    private System.Random random = new System.Random();
+    //랜덤 시스템 관련
+
+    //라인 보여주는 함수
+    private LineRenderer lineRenderer;
+    public float rayLength = 10f;
+    [SerializeField]
+    private GameObject targetObject;
+    //라인을 보여주는 관련
+
+    //조이스틱 관련
+    public VariableJoystick variableJoystick;
+    //조이스틱 관련
+
+    //다음 나올 과일을 보여주는 관련
+    [SerializeField]
+    private TextMeshProUGUI textMeshProUGUI;
+    private int nextTypeSphere;
+    //다음 나올 과일을 보여주는 관련
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextTypeSphere = 10;
+        mainCamera = Camera.main;
+        mCameraScript = mainCamera.GetComponent<SpaceCameraMoveScript>();
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody>();
+        minX = -1.8f;
+        maxX = 1.8f;
+        minZ = -1.8f;
+        maxZ = 1.8f;
+        lineRenderer = GetComponent<LineRenderer>();
+        //FirstSettingSphereMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (lineRenderer != null)
+        {
+            lineRenderer.enabled = (heldSphere != null);
+            targetObject.SetActive((heldSphere != null));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && heldSphere != null && !isReleasing)
+        {
+            StartCoroutine(ReleaseSphereWithDelay());
+        }
+
+        if (lineRenderer != null && lineRenderer.enabled)
+        {
+            DrawTrajectory();
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (_isMoving && mainCamera != null)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            x += variableJoystick.Horizontal;
+            z += variableJoystick.Vertical;
+
+            if (x != 0 || z != 0)
+            {
+                if (mCameraScript.GetCurrentCameraIndex() == 0)
+                {
+                    Vector3 movement = new Vector3(x, 0, z) * moveSpeed * Time.deltaTime;
+                    transform.Translate(movement, Space.World);
+                    float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+                    float clampedZ = Mathf.Clamp(transform.position.z, minZ, maxZ);
+                    transform.position = new Vector3(clampedX, transform.position.y, clampedZ);
+                }
+                else
+                {
+                    Vector3 forward = mainCamera.transform.forward;
+                    Vector3 right = mainCamera.transform.right;
+                    forward.y = 0; // Keep the movement horizontal
+                    right.y = 0;
+                    forward.Normalize();
+                    right.Normalize();
+
+                    Vector3 movement = (forward * z + right * x) * moveSpeed * Time.deltaTime;
+
+                    // Apply movement
+                    transform.Translate(movement, Space.World);
+
+                    // Clamp position
+                    float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+                    float clampedZ = Mathf.Clamp(transform.position.z, minZ, maxZ);
+                    transform.position = new Vector3(clampedX, transform.position.y, clampedZ);
+                }
+
+            }
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject != null && heldSphere != null && collision.gameObject != heldSphere.gameObject)
+        {
+            Debug.Log("TongsMoveScript OnCollisionEnter(),collision.gameObject != null && heldSphere != null && collision.gameObject != heldSphere.gameObject ");
+            SpaceGameManager.Instance.GameOver();
+        }
+    }
+
+    private void SettingSphereMove()
+    {
+        if (heldSphere == null)
+        {
+            SpaceSphereScript sphere = SpaceGameManager.Instance.GetObject(nextTypeSphere);
+            HoldSphere(sphere);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void FirstSettingSphereMove()//첫번째만 쓰이는 용도
+    {
+        if (heldSphere == null)
+        {
+            SpaceSphereScript sphere = SpaceGameManager.Instance.GetObject(0);
+            HoldSphere(sphere);
+        }
+        NextSphererInforMation();
+    }
+
+    public void DropSphere()
+    {
+        if (heldSphere != null && canDropSphere)
+        {
+            canDropSphere = false;
+            StartCoroutine(ResetDropSphere());
+            float offsetX = (float)(random.NextDouble() * 0.02 - 0.01); // Random value between -0.01 and 0.01
+            float offsetZ = (float)(random.NextDouble() * 0.02 - 0.01); // Random value between -0.01 and 0.01
+            Vector3 newPosition = this.transform.position + new Vector3(offsetX, 0, offsetZ);
+            heldSphere.transform.position = newPosition;
+
+            heldSphere.SetTarget(null);
+            Rigidbody sphereRb = heldSphere.GetComponent<Rigidbody>();
+            if (sphereRb != null)
+            {
+                sphereRb.isKinematic = false; // Enable gravity (if Rigidbody is used)
+            }
+
+            SphereCollider sphereCollider = heldSphere.GetComponent<SphereCollider>();
+            if (sphereCollider != null)
+            {
+                sphereCollider.enabled = true;
+            }
+
+            heldSphere = null; // Clear the reference
+        }
+    }
+    private IEnumerator ResetDropSphere()
+    {
+        yield return new WaitForSeconds(0.2f);
+        canDropSphere = true;
+    }
+    public void HoldSphere(SpaceSphereScript sphere)
+    {
+        if (heldSphere != null)
+        {
+            // Optionally, release the currently held sphere
+            // Similar to the spacebar logic
+        }
+
+        heldSphere = sphere;
+        heldSphere.SetTarget(this.transform);
+        Rigidbody sphereRb = heldSphere.GetComponent<Rigidbody>();
+        if (sphereRb != null)
+        {
+            sphereRb.isKinematic = true; // Disable gravity (if Rigidbody is used)
+        }
+    }
+
+
+    public int GetRandomNumber()//점수에 따른 스폰
+    {
+        if (SpaceGameManager.Instance.Score >= 2000)//2천점 이상일 때 
+        {
+            int randomNumber = random.Next(0, 100); // Random number between 0 and 99
+
+            if (randomNumber < 30) // 30% probability for 1
+            {
+                return 1;
+            }
+            else if (randomNumber < 60) // Additional 30% probability for 2 (30% + 30%)
+            {
+                return 2;
+            }
+            else if (randomNumber < 80) // Additional 20% probability for 3 (30% + 30% + 20%)
+            {
+                return 3;
+            }
+            else if (randomNumber < 90) // Additional 10% probability for 4 (30% + 30% + 20% + 10%)
+            {
+                return 4;
+            }
+            else // Remaining 10%
+            {
+                return 0;
+            }
+        }
+        else //2천점 미만 
+        {
+            int randomNumber = random.Next(0, 100); // Random number between 0 and 99
+
+            if (randomNumber < 40) // 0 나올 확률 40% 
+            {
+                return 0;
+            }
+            else if (randomNumber < 70) // 30% probability (40% + 30%) 1나올 확률 30%
+            {
+                return 1;
+            }
+            else if (randomNumber < 90) // 20% probability (40% + 30% + 20%) 2나올 확률 20%
+            {
+                return 2;
+            }
+            else if (randomNumber < 98) // 8% probability (40% + 30% + 20% + 8%) 3나올 확률 8% 
+            {
+                return 3;
+            }
+            else // Remaining 2% 4나올 확률 2%
+            {
+                return 4;
+            }
+        }
+    }
+
+    void DrawTrajectory()
+    {
+        Vector3 start = transform.position;
+        Vector3 direction = Vector3.down;
+        lineRenderer.SetPosition(0, start);
+
+        if (Physics.Raycast(start, direction, out RaycastHit hit, rayLength))
+        {
+            Vector3 hitPoint = hit.point;
+
+            targetObject.transform.position = hitPoint;
+
+            SetLineRendererGradientAtPoint(hit.distance / rayLength);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            ResetLineRendererGradient();
+            lineRenderer.SetPosition(1, start + direction * rayLength);
+        }
+    }
+
+    void SetLineRendererGradientAtPoint(float relativePoint)
+    {
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.red, relativePoint), new GradientColorKey(Color.red, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, relativePoint), new GradientAlphaKey(1.0f, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+    }
+
+    void ResetLineRendererGradient()
+    {
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.white, 0.0f), new GradientColorKey(Color.white, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+    }
+
+    public void NextSphererInforMation() // 다음 구체에 대한 정보
+    {
+        SettingSphereMove();
+
+        //nextTypeSphere = GetRandomNumber();
+        nextTypeSphere -= 1;
+        textMeshProUGUI.text = nextTypeSphere + ": Next";
+    }
+
+    private IEnumerator ReleaseSphereWithDelay()
+    {
+        isReleasing = true;
+        yield return new WaitForSeconds(0.1f); // Delay of 0.2 seconds
+
+        // Existing logic for releasing the sphere
+        float offsetX = (float)(random.NextDouble() * 0.02 - 0.01);
+        float offsetZ = (float)(random.NextDouble() * 0.02 - 0.01);
+        Vector3 newPosition = this.transform.position + new Vector3(offsetX, 0, offsetZ);
+        heldSphere.transform.position = newPosition;
+
+        heldSphere.SetTarget(null);
+        Rigidbody sphereRb = heldSphere.GetComponent<Rigidbody>();
+        if (sphereRb != null)
+        {
+            sphereRb.isKinematic = false;
+        }
+        SphereCollider sphereCollider = heldSphere.GetComponent<SphereCollider>();
+        if (sphereCollider != null)
+        {
+            sphereCollider.enabled = true;
+        }
+
+        heldSphere = null;
+
+        isReleasing = false; // Reset the flag
     }
 }
