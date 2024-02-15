deb100007d6c9301292b65d56da4652cd1dccb7c
KwonCheolHyeon <ch970314@naver.com>
2024-02-15 15:26:57 +0900

    카메라 터치 감도 조절

--- Assets/Scripts/SlimeScene/SlimeTongsMoveScript.cs
+++ Assets/Scripts/SlimeScene/SlimeTongsMoveScript.cs
@@ -181,7 +181,7 @@ public class SlimeTongsMoveScript : MonoBehaviour
         }
         else
         {
-            SlimeGameManager.Instance.GameOver();
+            //SlimeGameManager.Instance.GameOver();
         }
     }
 
--- Assets/Scripts/SpaceScene/SpaceTongsMoveScript.cs
+++ Assets/Scripts/SpaceScene/SpaceTongsMoveScript.cs
@@ -178,7 +178,7 @@ public class SpaceTongsMoveScript : MonoBehaviour
         }
         else
         {
-
+            
         }
     }
 
